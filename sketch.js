d7e0229d86feccd3fd22816d61df95f55f49c3d7
shaonli8 <72174470+shaonli8@users.noreply.github.com>
2021-03-16 13:02:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var w1, w2, w3, w4, w5, w6, w7, w8, w9, w10, w11, w12, w13, w14, w15, w16, w17, w18, w19, w20, w21, w22, w23, w24, w25, w26, w27;
+var thief, cop1, cop2
+var diamond
+
+
+function preload(){
+
+}
+
+function setup(){
+    createCanvas(600,600)
+
+    w1 = createSprite(300, 2, 300, 5)
+    w2 = createSprite(50, 2, 100, 5)
+    w3 = createSprite(550, 2, 100, 5)
+    w4 = createSprite(300, 598, 600, 5)
+    w5 = createSprite(2, 300, 5, 600)
+    w6 = createSprite(598, 200, 5, 400)
+    w7 = createSprite(598, 550, 5, 100)
+    w8 = createSprite(190, 60, 200, 5)
+    w9 = createSprite(152, 232, 5, 350)
+    w10 = createSprite(115, 240, 70, 5)
+    w11= createSprite(458, 175, 150, 5)
+    w12 = createSprite(535, 225, 5, 105)
+    w13 = createSprite(397, 235, 280, 5)
+    w14 = createSprite(259.5, 265, 5, 60)
+    w15 = createSprite(545, 350, 110, 5)
+    w16 = createSprite(488, 375, 5, 100)
+    w17 = createSprite(440, 333, 5, 60)
+    w18 = createSprite(378, 300, 130, 5)
+    w19 = createSprite(330, 365, 226, 5)
+    w20 = createSprite(410,425, 250, 5)
+    w21 = createSprite(352, 117, 250, 5)
+    w22 = createSprite(480, 90, 5, 60)
+    w23 = createSprite(40, 150, 80, 5)
+    w24 = createSprite(77, 250, 5, 200)
+    w25 = createSprite(75, 475, 180, 5)
+    w26 = createSprite(105, 410, 100, 5)
+    w27 = createSprite(80, 565, 5, 60)
+    w28 = createSprite(302, 535, 450, 5)
+    w29 = createSprite(525, 508, 5, 60)
+    w30 = createSprite(230, 145, 5, 60)
+    w31 = createSprite(288, 455, 5, 60)
+    w32 = createSprite(218, 390, 5, 56)
+    w33 = createSprite(452, 480, 150, 5)
+
+    thief = createSprite(124, 20, 12, 12)
+
+}
+
+function draw(){
+    
+
+    drawSprites()
+}
